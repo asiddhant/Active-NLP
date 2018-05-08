fixed broadcasting issue

neural_ner/models/cnn_cnn_lstm_mc.py
@@ -88,8 +88,8 @@ class CNN_CNN_LSTM_MC(nn.Module):
         new_word_features = torch.cat((word_features,word_input_feats),2)
         
         if score_only:
-            score, _ = self.decoder.decode(new_word_features, wordslen, usecuda=usecuda)
+            score, _ = self.decoder.decode(new_word_features, tagsmask, wordslen, usecuda=usecuda)
             return score
         
-        score, tag_seq = self.decoder.decode(new_word_features, wordslen, usecuda=usecuda)
+        score, tag_seq = self.decoder.decode(new_word_features, tagsmask, wordslen, usecuda=usecuda)
         return score, tag_seq

neural_ner/modules/DecoderRNN.py
@@ -23,7 +23,7 @@ class DecoderRNN(nn.Module):
         
         self.linear = nn.Linear(input_size, tag_size)
         self.ignore = -1
-        self.lossfunc = nn.CrossEntropyLoss(ignore_index= self.ignore)
+        self.lossfunc = nn.CrossEntropyLoss(ignore_index= self.ignore, size_average=False)
         
     def forward_step(self, input_var, prev_tag, hidden ,usecuda=True):
         
@@ -56,7 +56,7 @@ class DecoderRNN(nn.Module):
             loss += self.lossfunc(output, maskedtags[i])
         return loss
     
-    def decode(self, input_var, wordslen, usecuda=True):
+    def decode(self, input_var, mask, wordslen, usecuda=True):
         
         batch_size, sequence_len, _ = input_var.size()
         
@@ -73,10 +73,12 @@ class DecoderRNN(nn.Module):
             output, prev_tag, hidden=self.forward_step(input_var[i], prev_tag, hidden, 
                                                        usecuda=usecuda)
             tag_seq.append(prev_tag)
-            pb = nn.functional.softmax(output, dim = 1).data.cpu().numpy()
+            pb = nn.functional.log_softmax(output, dim = 1).data.cpu().numpy()
             probs.append(pb)
         
-        probs = np.array(probs).transpose(1,0,2)
+        probs = np.array(probs).transpose(1,0,2).max(2)
+        probs = probs * mask.cpu().data.numpy()
+        probs = probs.sum(1)
         
         tag_seq = np.array(tag_seq).transpose().tolist()
         tag_seq = [ts[:wordslen[i]] for i,ts in enumerate(tag_seq)]

neural_ner/util/evaluator.py
@@ -1,7 +1,7 @@
 import os
 import codecs
 import torch
-from utils import *
+from .utils import *
 import torch
 from torch.autograd import Variable
 

neural_ner/util/loader.py
@@ -5,7 +5,7 @@ torch.manual_seed(0)
 import torch.nn as nn
 from torch.nn import init
 from torch.autograd import Variable
-from utils import *
+from .utils import *
 import codecs
 import cPickle
 import itertools

neural_ner/util/trainer.py
@@ -1,14 +1,14 @@
 from __future__ import print_function
 from torch.autograd import Variable
 import time
-from evaluator import Evaluator
+from .evaluator import Evaluator
 import sys
 import os
 import numpy as np
 np.random.seed(0)
 import torch
 import torch.nn as nn
-from utils import *
+from .utils import *
 
 
 class Trainer(object):